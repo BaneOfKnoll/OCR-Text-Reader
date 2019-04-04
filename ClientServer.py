24df8e6c2254f8c566ce6170b81d10ff05d115b2
Tom <11151838+BaneOfKnoll@users.noreply.github.com>
2019-04-04 10:48:07 +1000

    Client server bugs fixed

--- ClientServer.py
+++ ClientServer.py
@@ -7,11 +7,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #connect to the port where the server
 server_address = ('localhost', 10000)
 print(sys.stderr, 'connecting to %s port %s'  % server_address)
-sock.connect(serve_address)
+sock.connect(server_address)
 
 try:
     #send data packet
-    message = 'this is the transmitted message'
+    message = 'this is the transmitted message'.encode()
     print(sys.stderr, 'sending "%s"' % message)
     sock.sendall(message)
 
@@ -25,5 +25,5 @@ try:
         print(sys.stderr, 'received "%s"' % data)
 
 finally:
-    print(sys.stderr, 'closing socket'
+    print(sys.stderr, 'closing socket')
     sock.close()
